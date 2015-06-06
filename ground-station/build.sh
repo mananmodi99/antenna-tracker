9dc9fe3bfb9750de6c1d6a5c167bbc2f878eeb04
Siim Raud <2ndalpha@gmail.com>
2015-06-06 19:34:08 +0300

    OS X support for building via command line

--- ground-station/build.sh
+++ ground-station/build.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-ARDUINO_FILE=arduino-${ARDUINO_IDE_VERSION}-linux64.$FILE_EXT
-wget -O $ARDUINO_FILE http://arduino.cc/download.php?f=/$ARDUINO_FILE
-tar xf $ARDUINO_FILE
-rm -f $ARDUINO_FILE
-echo "ARDUINO_DIR = arduino-${ARDUINO_IDE_VERSION}" >> Makefile
+ARDUINO_IDE_VERSION=1.6.4
+MAKEFILE_VERSION=1.5
 
+if [ $(uname) == 'Darwin' ]
+then
+    PLATFORM='macosx'
+    FILE_EXT='zip'
+    ARDUINO_DIR='Arduino.app/Contents/Java'
+else
+    PLATFORM='linux64'
+    FILE_EXT='tar.xz'
+    ARDUINO_DIR='arduino-$ARDUINO_IDE_VERSION'
+fi
+
+ARDUINO_FILE=arduino-${ARDUINO_IDE_VERSION}-$PLATFORM.$FILE_EXT
+wget -O $ARDUINO_FILE http://arduino.cc/download.php?f=/$ARDUINO_FILE
+
+if [ $FILE_EXT == 'zip' ]
+then
+    unzip $ARDUINO_FILE
+else
+    tar xf $ARDUINO_FILE
+fi
+
+rm -f $ARDUINO_FILE
+echo "ARDUINO_DIR = $ARDUINO_DIR" >> Makefile
 
 FILE=$MAKEFILE_VERSION.tar.gz
 
